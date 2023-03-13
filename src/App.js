e1f10c811613141c3dd321d75f3a4ef88a0b0c7a
GUTTI ERANNA <81354309+eranna-2940@users.noreply.github.com>
2023-03-13 18:34:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -68,28 +68,49 @@ function App() {
 
 const [data] = useState(listings);
 
-const getitem =(e)=>{
-
+const getitem =()=>
+{
   const prices = document.getElementById('price').value;
   const codes  = document.getElementById('code').value;
-  const result = document.getElementById('display')
-  const result1 = document.getElementById('message').value;
-
-  if(prices === '' && codes === ''){
-     alert('Enter price and code')
-  }else if(prices === '20' && codes === '1'){
-       let res = require('./images/d1.jpg')
-       result.innerHTML = `<img src=${res} width='100px' height='100px'/>`
-       result1.innerHTML = 'please take your item';
+  const result = document.getElementById('display');
+  const des = document.getElementById('des');
+   
+  const  quantity = document.getElementById('quantity').value;
+  if(prices === '' && codes === '' && quantity === ''){
+    alert('please enter the values')
   }
-}
+
+   data.map(e =>{
+        const qprice = e.price * quantity;
+       if(e.id.toString() === codes && qprice<=prices){
+          const remaingbal = prices - qprice;
+          result.innerHTML=`<img src=${e.image}  width='100px' height='100px' />
+           <p> Enter amount : ${prices} </p>
+           <p className='balance'> Remaining Balance : ${remaingbal} </p>
+           <span> Received : ${quantity} </span>
+  
+          `
+        }else if(e.id.toString() === codes && qprice>prices){
+          des.innerHTML  = 'insufficent funds'
+
+        }
+
+    
+        // if(e.id.toString() === codes && e.price.toString() === prices){
+        //   
+        // }
+        //  return result;
+   })
+
+
+
+} 
   return (
     <div className="App">
          <div className='main'>
          <h2>Vending Machine</h2>
           <div className='main'>
-
-                           
+         
             {
               data.map(({id,image,price}) =>(
                   <div className='card'>
@@ -110,11 +131,14 @@ const getitem =(e)=>{
               <input type='text' id='price' placeholder='enter a price' />
               <label>Enter Code </label>
               <input type='text' id='code' placeholder='enter a code' />
-              <button onClick={getitem}>GET</button>
-         
+              <label>Enter quatity </label>
+              <input type='num' placeholder='enter quantity' id='quantity'/>
+              <button onClick={getitem}>GET</button><br/>
+           <span id='des'>
+            
+           </span>
           <ul id='display'>
-             <p id='message'></p>
-          
+            
          </ul>
            </div>
     </div>
